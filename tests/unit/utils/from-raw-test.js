2e42da2e65ffa7f6169bee274d3d4ec8b29181ff
Imtiyaz <imtiyazs@mkcl.org>
2019-04-12 01:44:26 -0400

    Revert of deleted files

---
+++ tests/unit/utils/from-raw-test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import fromRaw from 'bcb-wallet/utils/from-raw';
+
+describe('Unit | Utility | from raw', () => {
+  // Replace this with your real tests.
+  it('works', () => {
+    const result = fromRaw('1000000000000000000000000000000');
+    expect(result).to.be.ok;
+  });
+});
